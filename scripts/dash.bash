e87e51e5536c39214077d1d64bea0bc3ed142612
Simeon Widdis <sawiddis@amazon.com>
2024-04-01 19:51:05 -0700

    Add dash up subcommand
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- scripts/dash.bash
+++ scripts/dash.bash
@@ -3,10 +3,15 @@
 ROOT=~/Documents/code/dashboards
 cd $ROOT
 
+if [ $1 == "up" ]; then
+    yarn osd bootstrap --single-version=loose
+    yarn start --no-base-path --host=0.0.0.0
+    exit 0
+fi
+
 git $@
 
-for dir in $ROOT/plugins/*/;
-do
+for dir in $ROOT/plugins/*/; do
     echo "=== $(basename $dir) ==="
     cd $dir
     git $@
