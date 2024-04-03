cc13ec4688a4510b47a204a6d7e1a11d500289f4
Simeon Widdis <sawiddis@amazon.com>
2024-04-03 08:34:28 -0700

    Fix dash up --no-bootstrap check
    Signed-off-by: Simeon Widdis <sawiddis@amazon.com>

--- scripts/dash.bash
+++ scripts/dash.bash
@@ -3,8 +3,23 @@
 ROOT=~/Documents/code/dashboards
 cd $ROOT
 
+# Helper method to check for param https://stackoverflow.com/a/56431189/7543069
+has_param() {
+    local term="$1"
+    shift
+    for arg; do
+        if [[ $arg == "$term" ]]; then
+            return 0
+        fi
+    done
+    return 1
+}
+
 if [ $1 == "up" ]; then
-    yarn osd bootstrap --single-version=loose
+    shift 1
+    if ! has_param "--no-bootstrap" "$@"; then
+        yarn osd bootstrap --single-version=loose
+    fi
     yarn start --no-base-path --host=0.0.0.0
     exit 0
 fi
